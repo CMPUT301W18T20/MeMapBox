21df605201abc31c083815ec816167424ce6bc52
Jolene Poulin <jcpoulin@ualberta.ca>
2018-03-26 16:35:22 -0600

    Added a Mapbox

--- MeMapBox/app/src/main/java/com/example/jpoulin/memapbox/MainActivity.java
+++ MeMapBox/app/src/main/java/com/example/jpoulin/memapbox/MainActivity.java
@@ -3,11 +3,61 @@ package com.example.jpoulin.memapbox;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+
 public class MainActivity extends AppCompatActivity {
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, "pk.eyJ1IjoiamNwb3VsaW4iLCJhIjoiY2pmOHFwYjI4MG5idjN1bG5ta2ozdjk2YSJ9.IAP7Id3hbU6U4-kA3G-8Ew");
         setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
 }
